3f5ace570f8dc8a9b380908375eaef77d51bba85
vuquang178 <zawarudo1782004@gmail.com>
2024-10-02 10:18:48 +0700

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChessGame from './ChessGame';
+
+function App() {
+  return (
+    <div >
+       <ChessGame/>
+    </div>
+  );
+}
+
+export default App;
